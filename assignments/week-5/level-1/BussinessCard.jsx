6fcd084d353292ff893929aaa3871a1e058593e6
shivamjaiswal123 <shivamj755@gmail.com>
2024-09-08 19:35:04 +0530

    week-5 done

---
+++ assignments/week-5/level-1/BussinessCard.jsx
@@ -0,0 +1,15 @@
+<div class="w-96 h-80 rounded-md bg-slate-300 p-4 font-mono">
+    <h1 class="text-4xl font-bold">Lokeshwar</h1>
+    <h2 class="text-xl mt-2.5">A TA in 100xDevs Cohort2.0</h2>
+    <p class="text-xl mt-8 font-bold">Interests</p>
+    <ul class="mt-4">
+      <li>Iconic</li>
+      <li>Open Source</li>
+      <li>App dev</li>
+    </ul>
+
+    <div class="flex gap-12 mt-6">
+      <a class="bg-blue-500 px-6 py-2 rounded-md text-white" href="">LinkedIn</a>
+      <a class="bg-blue-500 px-6 py-2 rounded-md text-white" href="">Twitter</a>
+    </div>
+</div>
